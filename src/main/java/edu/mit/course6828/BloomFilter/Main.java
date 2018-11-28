47c4cdee7ff243c69f3073ecfe266037bf9918b6
Janice <janicec124@yahoo.com>
2018-11-27 19:46:24 -0500

    work on normal fpr

--- src/main/java/edu/mit/course6828/BloomFilter/Main.java
+++ src/main/java/edu/mit/course6828/BloomFilter/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	public static Set<String> getUrls(boolean badOnly) throws IOException {
 		File file = new File("model_training/data.csv");
 		BufferedReader reader = new BufferedReader(new FileReader(file));
-		Set<String> lines = new HashSet<>();
+		Set<String> lines = new HashSet<String>();
 		int maxBits = Integer.MIN_VALUE;
 		int minBits = Integer.MAX_VALUE;
 		try {
@@ -23,12 +23,19 @@ public class Main {
 			
 			while(line != null) {
 				if (!badOnly || (badOnly && line.endsWith("bad"))) {
-					lines.add(line);
+					String url = line;
+					if (line.endsWith("good")) {
+						url = line.substring(0, line.lastIndexOf("good"));
+					} else if (line.endsWith("bad")) {
+						url = line.substring(0, line.lastIndexOf("bad"));
+					}
+					lines.add(url);
 					maxBits = Math.max(maxBits, line.getBytes().length * 8);
 					minBits = Math.min(minBits, line.getBytes().length * 8);
 				}
 				line = reader.readLine();
 			}
+			System.out.println("number of URLs (bad only = " + badOnly + "): " + lines.size());
 			System.out.println("bits: " + minBits + ", " + maxBits);
 			return lines;
 		} catch (IOException e) {
@@ -36,14 +43,13 @@ public class Main {
 		} finally {
 			reader.close();
 		}
-		
 	}
 
 	public static void main(String[] args) throws IOException {
-		int m = 180000;
-		int k = 30;
-		int bitsMin = 5; // 40 for data, 14 example
-		int bitsMax = 15; // 18488 for data, 23 example
+		int m = 3941837;
+		int k = 7;
+		int bitsMin = 40; // 40 for data, 14 example
+		int bitsMax = 18488; // 18488 for data, 23 example
 		
 		Random r = new SecureRandom();
 		Set<String> badUrls = getUrls(true);
@@ -54,7 +60,8 @@ public class Main {
 
 		for (int log2bits = bitsMin; log2bits <= bitsMax; log2bits++) {
 		  System.out.println("log2bits: " + log2bits);
-		  for (int noHashes = 29; noHashes <= k; noHashes++) {
+		  
+		  for (int noHashes = 6; noHashes <= k; noHashes++) {
 			  System.out.println("log2bits: " + log2bits + ", noHashes: " + noHashes);
 		    double noFalsePositives = 0;
 		    int noNotIn = 0;
@@ -83,8 +90,8 @@ public class Main {
 		    }
 		    double falsePositiveRate = noNotIn == 0 ? 0d :
 		      noFalsePositives / noNotIn;
-//		    System.out.println(noNotIn + ", " + noFalsePositives);
-		    System.out.println(falsePositiveRate);
+		    System.out.println("number not in: " + noNotIn + ", number false pos: " + noFalsePositives);
+		    System.out.println("fpr: " + falsePositiveRate);
 		    
 		    FileOutputStream outputStream = new FileOutputStream("bfWrite.txt");
 		    bf.writeTo(outputStream);
